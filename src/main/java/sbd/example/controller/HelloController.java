4f219f2bf130e615466ddaaed499b713b0db0d25
sionkim0126 <kimsion000@naver.com>
2024-12-28 21:47:36 +0900

    API사용

--- src/main/java/sbd/example/controller/HelloController.java
+++ src/main/java/sbd/example/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -19,5 +20,12 @@ public class HelloController {
         public String helloMvc(@RequestParam("sion")/*sion은 param 곧 통로(키네임)*/ String name, Model model){
             model.addAttribute("name", name);//name=키값
             return "hello-template";
-        }
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name")String name){
+        return "hello " + name;
+    }
+
 }
