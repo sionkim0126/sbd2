7f9ef0139f0ceeacdf2e94cef349c52cc7553f37
sionkim0126 <kimsion000@naver.com>
2024-12-28 21:23:12 +0900

    MVC 가동템플릿 확인 *model은 view와 controller를 이어주는 컨테이너 역할*

--- src/main/java/sbd/example/controller/HelloController.java
+++ src/main/java/sbd/example/controller/HelloController.java
@@ -3,6 +3,7 @@ package sbd.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -13,4 +14,10 @@ public class HelloController {
         model.addAttribute("data", "Hello!!");
         return "hello";
     }
+
+    @GetMapping("/hello-mvc")
+        public String helloMvc(@RequestParam("sion")/*sion은 param 곧 통로(키네임)*/ String name, Model model){
+            model.addAttribute("name", name);//name=키값
+            return "hello-template";
+        }
 }
