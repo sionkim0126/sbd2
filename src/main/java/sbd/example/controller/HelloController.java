19e143ed139c34c74c2dcc4bce2e9f41b381c095
sionkim0126 <kimsion000@naver.com>
2025-01-02 21:25:09 +0900

    API사용(객체사용)

--- src/main/java/sbd/example/controller/HelloController.java
+++ src/main/java/sbd/example/controller/HelloController.java
@@ -25,7 +25,31 @@ public class HelloController {
     @GetMapping("hello-string")
     @ResponseBody
     public String helloString(@RequestParam("name")String name){
-        return "hello " + name;
+        return "hello " + name;//?name=sion => hello sion
     }
+
+    @GetMapping("hello-api")
+    @ResponseBody//템블릿은 맞는 템플릿을 찾아 보내지만 responseBady는 HttpMassageConverter중 StringConverter작동하여 덤짐
+                 //객체가 넘어오면 JsonConverter로 작동하여
+    public Hello helloApi(@RequestParam("key") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+    }
+
 
 }
