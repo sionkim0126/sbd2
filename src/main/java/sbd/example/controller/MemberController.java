b69d5504784d7cd60cff03979e5f4d7f6d78d4ff
sionkim0126 <kimsion000@naver.com>
2025-01-08 15:39:50 +0900

    회원조회 기능 추가/ 속성전송 및 키값 조회

--- src/main/java/sbd/example/controller/MemberController.java
+++ src/main/java/sbd/example/controller/MemberController.java
@@ -2,10 +2,13 @@ package sbd.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sbd.example.domain.Member;
 import sbd.example.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -18,15 +21,25 @@ public class MemberController {
     }
     @GetMapping("members/new")
     public String createForm(){
+        System.out.println("1");
         return "members/createMemberForm";
     }
 
     @PostMapping("members/new")
     public String create(MemberForm form){
+        System.out.println("2");
         Member member = new Member();
         member.setName(form.getName());
 
         memberService.join(member);
+        System.out.println(member.getName());
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
